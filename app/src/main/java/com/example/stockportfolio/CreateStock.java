8534e5d215c4c2be06e8bf978452359f2d181566
Eric Nguyen <enguyen2@yahoo.com>
2020-08-02 20:32:56 -0700

    setup input stock screen

---
+++ app/src/main/java/com/example/stockportfolio/CreateStock.java
@@ -0,0 +1,14 @@
+package com.example.stockportfolio;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateStock extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_stock);
+    }
+}
